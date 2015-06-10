b61e5a00577054f8e6db6b5320bf670fecd44a0b
tahivan <thainv@smartosc.com>
2015-06-11 04:02:22 +0700

    updae

--- app/code/local/SM/Customize/Helper/Data.php
+++ app/code/local/SM/Customize/Helper/Data.php
@@ -1,10 +1,20 @@
 <?php
+
 /**
  * Created by PhpStorm.
  * User: VanThai
  * Date: 6/9/2015
  * Time: 1:18 AM
- */ 
-class SM_Customize_Helper_Data extends Mage_Core_Helper_Abstract {
+ */
+class SM_Customize_Helper_Data extends Mage_Core_Helper_Abstract
+{
+    public function getCheckoutUrl()
+    {
+        return $this->_getUrl('checkout/');
+    }
 
+    public function getBlogUrl()
+    {
+        return $this->_getUrl('blog/');
+    }
 }
