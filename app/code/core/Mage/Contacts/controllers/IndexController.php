976523c198da8edd1e9c7942de7ac24200f9a4b3
tahivan <thainv@smartosc.com>
2015-07-23 07:40:49 +0700

    fix contact page title

--- app/code/core/Mage/Contacts/controllers/IndexController.php
+++ app/code/core/Mage/Contacts/controllers/IndexController.php
@@ -51,6 +51,7 @@ class Mage_Contacts_IndexController extends Mage_Core_Controller_Front_Action
     public function indexAction()
     {
         $this->loadLayout();
+        $this->getLayout()->getBlock('head')->setTitle($this->__('Contacts'));
         $this->getLayout()->getBlock('contactForm')
             ->setFormAction( Mage::getUrl('*/*/post') );
 
