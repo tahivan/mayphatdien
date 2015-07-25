5cf121ab3c596a187a49ca702f03064f1ffba83e
tahivan <thainv@smartosc.com>
2015-07-25 12:46:23 +0700

    update css

--- app/code/local/Magestore/Featuredproduct/Block/Featuredproduct.php
+++ app/code/local/Magestore/Featuredproduct/Block/Featuredproduct.php
@@ -15,7 +15,7 @@ class Magestore_Featuredproduct_Block_Featuredproduct extends Mage_Catalog_Block
             ->addAttributeToFilter('fb_product', array('eq' => '1'))
 			->addStoreFilter()
             ->setOrder('feature_position', 'ASC');
-    
+
 		Mage::getSingleton('catalog/product_status')->addVisibleFilterToCollection($products);
 		//comment by viet update version magento CM 1.9.0.1
 		//Mage::getSingleton('catalog/product_visibility')->addVisibleInSearchFilterToCollection($products);
